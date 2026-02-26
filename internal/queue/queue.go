70520157fa8f0fc02d9f016451d38dd01d8fe458
Stepan <stephan.z@mail.ru>
2026-02-26 19:29:52 +0300

    Added thread safety to the queue

--- internal/queue/queue.go
+++ internal/queue/queue.go
@@ -1,17 +1,50 @@
 package queue
 
+import (
+	"sync"
+)
+
 type Queue struct {
-	elements []map[string]interface{}
+	mutex    sync.Mutex
+	cond     *sync.Cond
+	messages []map[string]interface{}
 }
 
 func NewQueue() *Queue {
-	return &Queue{make([]map[string]interface{}, 0)}
+	q := &Queue{messages: make([]map[string]interface{}, 0)}
+
+	q.cond = sync.NewCond(&q.mutex)
+
+	return q
 }
 
-func (q *Queue) AddElement(element map[string]interface{}) {
-	q.elements = append(q.elements, element)
+func (q *Queue) Add(message map[string]interface{}) {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+
+	q.messages = append(q.messages, message)
+
+	q.cond.Signal()
 }
 
-func (q *Queue) ElementsNumber() int {
-	return len(q.elements)
+func (q *Queue) Take() map[string]interface{} {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+
+	for len(q.messages) == 0 {
+		q.cond.Wait()
+	}
+
+	message := q.messages[0]
+
+	q.messages = q.messages[1:]
+
+	return message
+}
+
+func (q *Queue) GetQuantity() int {
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
+
+	return len(q.messages)
 }
